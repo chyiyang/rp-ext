a8d48c8063d094c70163d9d71b95176b3139f98e
Ing <wjz304@qq.com>
2023-08-04 13:36:01 +0800

    add i915le10th,nvmecache,nvmevolume

---
+++ addons/nvmecache/releases/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env ash
+
+if [ `mount | grep tmpRoot | wc -l` -gt 0 ] ; then
+  HASBOOTED="yes"
+  echo "System passed junior"
+else
+  echo "System is booting"
+  HASBOOTED="no"
+fi
+
+if [ "$HASBOOTED" = "yes" ]; then
+  echo "Installing daemon for nvmecache"
+  cp -v nvmecache.sh /tmpRoot/usr/bin/nvmecache.sh
+
+  DEST="/tmpRoot/usr/lib/systemd/system/nvmecache.service"
+  echo "[Unit]"                                    >${DEST}
+  echo "Description=Enable M2 Cache"              >>${DEST}
+  echo "After=multi-user.target"                  >>${DEST}
+  echo                                            >>${DEST}
+  echo "[Service]"                                >>${DEST}
+  echo "Type=oneshot"                             >>${DEST}
+  echo "RemainAfterExit=true"                     >>${DEST}
+  echo "ExecStart=/usr/bin/nvmecache.sh"          >>${DEST}
+  echo                                            >>${DEST}
+  echo "[Install]"                                >>${DEST}
+  echo "WantedBy=multi-user.target"               >>${DEST}
+
+  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/nvmecache.service /tmpRoot/lib/systemd/system/multi-user.target.wants/nvmecache.service
+fi
---
+++ addons/nvmevolume/releases/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env ash
+
+if [ `mount | grep tmpRoot | wc -l` -gt 0 ] ; then
+  HASBOOTED="yes"
+  echo "System passed junior"
+else
+  echo "System is booting"
+  HASBOOTED="no"
+fi
+
+if [ "$HASBOOTED" = "yes" ]; then
+  echo "Installing daemon for nvmevolume"
+  cp -v bc /tmpRoot/usr/bin/bc
+  cp -v nvmevolume.sh /tmpRoot/usr/bin/nvmevolume.sh
+
+  DEST="/tmpRoot/usr/lib/systemd/system/nvmevolume.service"
+  echo "[Unit]"                                    >${DEST}
+  echo "Description=Enable M2 volume"             >>${DEST}
+  echo "After=multi-user.target"                  >>${DEST}
+  echo                                            >>${DEST}
+  echo "[Service]"                                >>${DEST}
+  echo "Type=oneshot"                             >>${DEST}
+  echo "RemainAfterExit=true"                     >>${DEST}
+  echo "ExecStart=/usr/bin/nvmevolume.sh"         >>${DEST}
+  echo                                            >>${DEST}
+  echo "[Install]"                                >>${DEST}
+  echo "WantedBy=multi-user.target"               >>${DEST}
+
+  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/nvmevolume.service /tmpRoot/lib/systemd/system/multi-user.target.wants/nvmevolume.service
+fi
