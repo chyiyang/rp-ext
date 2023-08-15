4707aa385392be3f6b9018a5c5f2a5d3dda50248
Ing <wjz304@qq.com>
2023-08-16 03:36:44 +0800

    add setrootpw,addincards

---
+++ addons/addincards/releases/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if [ $(mount | grep tmpRoot | wc -l) -gt 0 ]; then
+  HASBOOTED="yes"
+  echo "System passed junior"
+else
+  echo "System is booting"
+  HASBOOTED="no"
+fi
+
+if [ "$HASBOOTED" = "yes" ]; then
+  echo "Installing daemon for addincards"
+  cp -v addincards.sh /tmpRoot/usr/bin/addincards.sh
+
+  DEST="/tmpRoot/usr/lib/systemd/system/addincards.service"
+  echo "[Unit]"                                        >${DEST}
+  echo "Description=update Add-in cards and usb.map"  >>${DEST}
+  echo "After=multi-user.target"                      >>${DEST}
+  echo                                                >>${DEST}
+  echo "[Service]"                                    >>${DEST}
+  echo "Type=oneshot"                                 >>${DEST}
+  echo "RemainAfterExit=true"                         >>${DEST}
+  echo "ExecStart=/usr/bin/addincards.sh"             >>${DEST}
+  echo                                                >>${DEST}
+  echo "[Install]"                                    >>${DEST}
+  echo "WantedBy=multi-user.target"                   >>${DEST}
+
+  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/addincards.service /tmpRoot/lib/systemd/system/multi-user.target.wants/addincards.service
+fi
